13b1e0e6a4fac05480127a0faa5f0901ed798d6f
Miguel Angel <m_a_gumiel@hotmail.com>
2025-04-17 18:25:50 +0200

    buscador-libros

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// Importamos la función de configuración de Vite y el plugin de React
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// Exportamos la configuración para Vite
 export default defineConfig({
-  plugins: [react()],
-})
+  // 👇 Esta propiedad base es MUY IMPORTANTE para que funcione el despliegue en GitHub Pages
+  // Debe ser igual al nombre del repositorio si usas GitHub Pages
+  base: '/buscador-libros/',
+
+  plugins: [react()]
+});
