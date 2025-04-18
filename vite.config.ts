c51768578139d078b9882dafee4f5cdde59cc772
Miguel Angel <m_a_gumiel@hotmail.com>
2025-04-18 17:32:49 +0200

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-// Importamos la función de configuración de Vite y el plugin de React
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Exportamos la configuración para Vite
 export default defineConfig({
-  // 👇 Esta propiedad base es MUY IMPORTANTE para que funcione el despliegue en GitHub Pages
-  // Debe ser igual al nombre del repositorio si usas GitHub Pages
-  base: '/buscador-libros/',
-
-  plugins: [react()]
+  plugins: [react()],
+  base: './', // 👈 esta es la correcta para Netlify
 });
+
